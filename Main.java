bf21ec83617fa8a8131c5057b2a6e9a0b095549f
Nurdin954 <84643087+Nurdin954@users.noreply.github.com>
2021-06-06 11:28:56 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+	public static void main(String[] args) {
+		double x = 12;    
+        double y = 42;    
+      
+        System.out.println("Maximum number of x and y is: " +Math.max(x, y));  
+		// возвращаем максимум два числа   		
+           
+        System.out.println("Square root of y is: " + Math.sqrt(y));  
+		// возвращаем квадратный корень из y		
+                  
+        System.out.println("Logarithm of x is: " + Math.log(x));   
+        System.out.println("Logarithm of y is: " + Math.log(y)); 
+		// возвращаем логарифм заданного значения       		
+
+        System.out.println("exp of a is: " +Math.exp(x)); 
+		// возвращаем степень 2    
+           
+        System.out.println("expm1 of a is: " +Math.expm1(x));  
+		// возврат (степень двойки) -1  
+
+
+	
+	}
+}
